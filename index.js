86a2a4985a44b131dd956cc850c4aba847afdc53
Jeff Schwartz <jefftschwartz@gmail.com>
2018-04-24 14:05:49 -0400

    renamed userConfig to config

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 #!/usr/bin/env node
 
-const config = require("./lib/config");
+const configFactory = require("./lib/config");
 const manifestFactory = require("./lib/manifest");
 
-// a mock for a config, a package.json hash or command line arguments
-// const directives = [
-//     "test-project/media/alphabet-arts-and-crafts-blog-459688-worked.jpg:1:test-project/media",
-//     "test-project/media/black-and-white-close-up-cobweb-worked.jpg:1:test-project/media",
-//     "test-project/media/cyclone-roller-coaster-coney-island-worked.jpg:1:test-project/media",
-//     "test-project/media/tatoo-handshake-worked.jpg:1:test-project/media",
-//     "test-project/index.html:2:test-project",
-//     "test-project/css/test.css:3:test-project/css",
-//     "test-project/script/test.js:3:test-project/script"
-// ];
-
-// const config = {
-//     directives: directives,
-//     options: {
-//         // where to save the manifest, defaults to cwd
-//         manifestDest: "./manifest.json",
-//         // rename backup files  and delete generated hash files
-//         restore: true,
-//         // dry run only
-//         dryRun: false
-//     }
-// };
-
 const processBuster = () => {
-    console.log("processing started");
-    const userConfig = config();
+    const config = configFactory();
 
-    if (!userConfig) {
+    console.log("processing started");
+
+    if (!config) {
         console.log("no valid configuration found");
         console.log("terminating process");
         process.exit();
     }
 
-    // manifestFactory()(config.directives, config.options);
-    manifestFactory()(userConfig.directives, userConfig.options);
+    manifestFactory()(config.directives, config.options);
+
     console.log("processing completed");
 };
 
