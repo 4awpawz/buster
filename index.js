e6eb03b34b2b0e08441786b6b7c50fb454d56b54
Jeff Schwartz <jefftschwartz@gmail.com>
2018-05-06 16:00:29 -0400

    working glob integration

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const configFactory = require("./lib/config");
 const manifestFactory = require("./lib/manifest");
+const expandOds = require("./lib/ods");
 
 const processBuster = async (paramsConfig = {}) => {
     const config = await configFactory(paramsConfig);
+    var expandedOds;
 
     console.log("processing started");
 
@@ -12,7 +14,11 @@ const processBuster = async (paramsConfig = {}) => {
         process.exit();
     }
 
-    await manifestFactory()(config.directives, config.options);
+    expandedOds = await expandOds(config.directives);
+    console.log("expandedOds", expandedOds);
+
+    // await manifestFactory()(config.directives, config.options);
+    await manifestFactory()(expandedOds, config.options);
 
     console.log("processing completed");
 };
