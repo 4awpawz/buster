abc4066ca3d6102b8c36aba206eb99e542a6701f
Jeff Schwartz <jefftschwartz@gmail.com>
2019-09-15 21:59:55 -0400

    removed replace-in-file package, updated package dependencies, now using new format when creating hashed file names, implemented replacement for replace-in-file, reduced console.log output, see issues #14, #15 & #16

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const configFactory = require("./lib/config");
 const manifestFactory = require("./lib/manifest");
 const expandOds = require("./lib/ods");
+const log = require("./lib/utils/log");
 
 const processBuster = async (paramsConfig) => {
     const config = await configFactory(paramsConfig);
     const expandedOds = await expandOds(config.directives, config.options.ignore);
 
-    console.log("processing started");
+    log("processing started");
 
     await manifestFactory(config.command, expandedOds, config.options);
 
-    console.log("processing completed");
+    log("processing completed");
 };
 
 module.exports = processBuster;
